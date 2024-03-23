4acdf2a0514cd1509356037d950c694a2a79fd5e
Palak Bhawsar <69889600+palakbhawsar98@users.noreply.github.com>
2024-03-23 21:13:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "test_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  
   instance_type = "t2.micro"
 
+metadata_options {
+    http_tokens = "required"
+  }
+
  root_block_device {
     volume_type           = "gp2"
     volume_size           = 2 
