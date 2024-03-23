79b1b54601db64b254a0b99239c60251e6868eac
Palak Bhawsar <69889600+palakbhawsar98@users.noreply.github.com>
2024-03-23 21:10:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@
 resource "aws_instance" "test_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  
   instance_type = "t2.micro"
+
+ root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 2 
+    encrypted             = true 
+  }
+
+  ebs_block_device {
+    device_name           = "/dev/sdb"
+    volume_type           = "gp2"
+    volume_size           = 2  
+    encrypted             = true 
+  }
   tags = {
     name = "test"
     created_by = "Palak"
