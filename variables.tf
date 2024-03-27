3a5abc8285d1aea3b8f06e8899390b749d1a6642
Palak Bhawsar <69889600+palakbhawsar98@users.noreply.github.com>
2024-03-27 10:03:06 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "ami" {
+  type    = string
+  default = "ami-080e1f13689e07408"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "volume_type" {
+  type    = string
+  default = "gp2"
+}
+
+variable "volume_size" {
+  type    = number
+  default = 8
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    name       = "test"
+    created_by = "Palak"
+  }
+}
+
+
